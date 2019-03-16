78ca6b4b3521696e78ecbc08c0fc316daff35101
dzieba93 <marcin.dziebowski@gmail.com>
2019-03-16 14:01:16 +0100

    style homepage

--- portfolio/src/components/home.js
+++ portfolio/src/components/home.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 class Home extends Component {
     render() {
         return(
             <div style={{width: '100%', margin: 'auto'}}>
-                Test Home
+                <Grid className="landing-grid">
+                    <Cell col={12}>
+                        <img
+                            src="./avatar.gif"
+                            alt="avatar"
+                            className="avatar-img"
+                        />
+
+                        <div className="banner-text">
+                            <h1>Software Developer</h1>
+
+                            <hr/>
+
+                            <p>HTML/CSS | Bootstrap | jQuery | MySQL | PHP | REST | Symfony | Laravel | React </p>
+
+                            <div className="social-links">
+                                {/* LinkedIn */}
+                                <a href="https://www.linkedin.com/in/marcin-dzi%C4%99bowski-135493158/" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-linkedin-square" aria-hidden="true" />
+                                </a>
+
+                                {/* Github */}
+                                <a href="https://github.com/dzieba93" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-github-square" aria-hidden="true" />
+                                </a>
+                            </div>
+                        </div>
+                    </Cell>
+                </Grid>
             </div>
         )
     }
--- portfolio/src/components/projects.js
+++ portfolio/src/components/projects.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 
 class Projects extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { activeTab: 0 };
+    }
     render() {
         return(
             <div>
-                Example projects
+              projects
             </div>
         )
     }
 }
+
 export default Projects;
